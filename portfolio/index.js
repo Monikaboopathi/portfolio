3becd615093c387ce30cccbc3906316a353fb9a8
Monikaboopathi <136053222+Monikaboopathi@users.noreply.github.com>
2023-08-31 07:51:30 +0530

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("nav").style.width = "60%";
+    document.getElementById("nav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("nav").style.display = "none";
+  }
+  
+  
